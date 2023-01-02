41245160cd4430bee632232ae45f2244d9d7c31b
Alina Markovich <alina-46@mail.ru>
2023-01-02 15:16:23 +0400

    First Homework

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿/* задача 2.
+Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.Write("First number " + num1 + " is bigger ");
+    Console.WriteLine("Second number " + num2 + " is smaller ");
+}
+else
+{
+    Console.Write("First number " + num2 + " is bigger ");
+    Console.WriteLine("Second number " + num1 + " is smaller ");
+}
+*/
+/* задача 4
+
+Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input a third number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if (max < num2)
+    max = num2;
+
+if (max < num3)
+    max = num3;
+{
+    Console.Write("The biggest number " + max);
+}
+*/
+
+/* Задача 6.
+
+Console.Write("Input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.Write("Number is even");
+}
+else
+{
+    Console.Write("Number is odd");
+}
+*/
+/* Задача 8.
+
+Console.Write("Input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int start = 0;
+int current = (start + 2);
+
+while (current <= number)
+{
+    Console.Write(current + ", ");
+    current = current + 2;
+}
+*/
